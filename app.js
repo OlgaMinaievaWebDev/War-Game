99da82040b3e0e4fab92cdb4746487ebdf85b103
Olga <minaeva9@gmail.com>
2024-01-31 14:01:13 -0500

    logic to draw cards added

--- app.js
+++ app.js
@@ -11,9 +11,14 @@ function getNewDeck() {
 
 document.getElementById("new-deck").addEventListener("click", getNewDeck);
 
-
-document.getElementById('draw-card').addEventListener('click', function () {
- fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
-   .then((res) => res.json())
-   .then((data) => console.log(data));
-})
+document.getElementById("draw-card").addEventListener("click", function () {
+  fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data.cards);
+      document.getElementById("cards").innerHTML = `
+       <img src=${data.cards[0].image} />
+       <img src=${data.cards[1].image} />
+   `;
+    });
+});
