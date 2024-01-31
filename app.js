6197a1954b3e452406485ca8a4ab5fbfa0882988
Olga <minaeva9@gmail.com>
2024-01-30 23:02:53 -0500

    draw card button added

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
+let deckId;
+
 function getNewDeck() {
   fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
     .then((res) => res.json())
-    .then((data) => console.log(data));
+    .then((data) => {
+      deckId = data.deck_id;
+      console.log(deckId);
+    });
 }
 
 document.getElementById("new-deck").addEventListener("click", getNewDeck);
+
+
+document.getElementById('draw-card').addEventListener('click', function () {
+ fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
+   .then((res) => res.json())
+   .then((data) => console.log(data));
+})
