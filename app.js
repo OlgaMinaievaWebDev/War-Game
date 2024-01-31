ff4b20ddee2a53a4b964c203d87f889555cba7e8
Olga <minaeva9@gmail.com>
2024-01-31 15:11:51 -0500

    add logic to determine winner

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 let deckId;
+const winnerTextEl = document.getElementById("winner-text");
 
 function getNewDeck() {
   fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
     .then((res) => res.json())
     .then((data) => {
       deckId = data.deck_id;
-      console.log(deckId);
     });
 }
 
@@ -15,10 +15,40 @@ document.getElementById("draw-card").addEventListener("click", function () {
   fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
     .then((res) => res.json())
     .then((data) => {
-      console.log(data.cards);
       document.getElementById("cards").innerHTML = `
        <img src=${data.cards[0].image} />
        <img src=${data.cards[1].image} />
    `;
+      const winnerText = determineCardWinner(data.cards[0], data.cards[1]);
+      winnerTextEl.textContent = winnerText;
     });
 });
+
+function determineCardWinner(card1, card2) {
+  const valueOptions = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "JACK",
+    "QUEEN",
+    "KING",
+    "ACE",
+  ];
+
+  const card1ValueIndex = valueOptions.indexOf(card1.value);
+  const card2ValueIndex = valueOptions.indexOf(card2.value);
+
+  if (card1ValueIndex > card2ValueIndex) {
+    return "Card 1 wins!";
+  } else if (card1ValueIndex < card2ValueIndex) {
+    return "Card 2 wins!";
+  } else {
+    return "War!";
+  }
+}
